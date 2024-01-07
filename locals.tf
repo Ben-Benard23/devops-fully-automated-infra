e456e1aeceb4a0790b627981c9381cd3400f90a2
“[Benard <ngwabern@gmail.com>
2024-01-07 01:03:52 -0600

    email changes

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags = {
     company    = "jjtech"
     owner      = "jjtech DevOps Team"
-    team-email = "team-devops@jjtech.com"
+    team-email = "ngwabern@gmail.com"
     time       = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
   }
 
